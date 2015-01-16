fc21050836f1fdc0189b2c91226b90ea2fd7b199
Maxim Khomutov <mkhomutov@gmail.com>
2015-01-16 16:24:48 +0300

    ForceRefreshWorkspacesBehavior moved from FG

---
+++ src/Orc.WorkspaceManagement.Xaml/Behaviors/Interfaces/IWorkspaceContainerLocator.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWorkspaceContainerLocator.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2015 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.WorkspaceManagement.Behaviors
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public interface IWorkspaceContainerLocator
+    {
+        Panel GetContainerByWorspaceParent(UIElement parent);
+    }
+}
