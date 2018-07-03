86973dabc9a27a329b1f32d0fe8c455681bed7c2
Geert van Horrik <geert@catenalogic.com>
2018-07-03 09:25:21 +0200

    Fix build

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.1")]
-[assembly: AssemblyCopyright("Copyright © WildGums 2010 - 2018")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha.1")]
+[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2018")]
 
