68b20399340aaf6e24eb1eb765abbf5d31bb2598
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:28:05 +0200

    Convert to file scoped namespaces

--- src/Orc.WorkspaceManagement.Xaml/OrcWorkspaceManagementXamlModule.cs
+++ src/Orc.WorkspaceManagement.Xaml/OrcWorkspaceManagementXamlModule.cs
@@ -1,18 +1,17 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcWorkspaceManagementXamlModule
 {
-    using Catel.Services;
-    using Microsoft.Extensions.DependencyInjection;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcWorkspaceManagementXamlModule
+    public static IServiceCollection AddOrcWorkspaceManagementXaml(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcWorkspaceManagementXaml(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.WorkspaceManagement.Xaml", "Orc.WorkspaceManagement.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.WorkspaceManagement.Xaml", "Orc.WorkspaceManagement.Properties", "Resources"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
